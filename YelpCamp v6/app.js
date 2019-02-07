58b387dabe5f2c607a7221dac6cd5629629b4b7d
Kartikeya Chauhan <kc1.408596@gmail.com>
2019-02-07 14:20:01 +0530

    Updated files

--- YelpCamp v6/app.js
+++ YelpCamp v6/app.js
@@ -27,6 +27,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get("/", function(req,res){
     res.render("landing");
 });
@@ -38,7 +43,7 @@ app.get("/campgrounds", function(req,res){
        if(err){
            console.log(err);
        } else {
-           res.render("campgrounds/index", {campgrounds:allCampgrounds});
+           res.render("campgrounds/index", {campgrounds:allCampgrounds, currentUser: req.user});
        }
     });
 });
@@ -87,7 +92,7 @@ app.get("/campgrounds/:id",function(req,res){
 // COMMENTS ROUTES
 // =======================
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Campground.findById(req.params.id, function(err, campground){
        if(err){
@@ -98,7 +103,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req,res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req,res){
     //lookup campground using ID
     Campground.findById(req.params.id, function(err,campground){
         if(err){
@@ -145,6 +150,31 @@ app.post("/register", function(req,res){
        }
     });
 });
+
+// show login form
+app.get("/login", function(req,res){
+   res.render("login"); 
+});
+//handling login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req,res){
+});
+
+//logout route
+app.get("/logout", function(req,res){
+   req.logout();
+   res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp running");
